8d3a881942c7d79352b487fd0703aeaaf5cca195
Hyun-Je Song <songhyunje@gmail.com>
2020-10-13 00:50:23 +0900

    Refactor dataset.py and data_module.py for new data format

--- data_module.py
+++ data_module.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
 from transformers import BertTokenizer
@@ -7,62 +9,61 @@ from table_bert import TableBertModel
 
 
 class QueryTableDataModule(pl.LightningDataModule):
-    def __init__(self,
-                 query_file: str = './data/queries.txt',
-                 table_file: str = './data/all.json',
-                 train_batch_size=16,
-                 valid_batch_size=16,
-                 test_batch_size=16
-                 ):
+    def __init__(self, params):
         super().__init__()
-        self.query_file = query_file
-        self.table_file = table_file
+        self.data_dir = params.data_dir
 
-        self.query_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        table_model = TableBertModel.from_pretrained('model/tabert_base_k3/model.bin')
+        self.query_tokenizer = BertTokenizer.from_pretrained(params.bert_path)
+        table_model = TableBertModel.from_pretrained(params.tabert_path)
         self.table_tokenizer = table_model.tokenizer
-        self.train_batch_size = train_batch_size
-        self.valid_batch_size = valid_batch_size
-        self.test_batch_size = test_batch_size
 
-    # def prepare_data(self):
-    #     # Download, tokenize, etc
-    #     # Write to disk or that need to be done only from a single GPU in distributed settings
-    #     QueryTableDataset(query_file=self.query_file,
-    #                       table_file=self.table_file,
-    #                       query_tokenizer=self.query_tokenizer,
-    #                       table_tokenizer=self.table_tokenizer,
-    #                       prepare=True
-    #                       )
+        self.train_batch_size = params.train_batch_size
+        self.valid_batch_size = params.valid_batch_size
+        if hasattr(params, 'test_batch_size'):
+            self.test_batch_size = params.test_batch_size
+
+    def prepare_data(self):
+        # Download, tokenize, etc
+        # Write to disk or that need to be done only from a single GPU in distributed settings
+        QueryTableDataset(data_dir=self.data_dir, data_type='train',
+                          query_tokenizer=self.query_tokenizer,
+                          table_tokenizer=self.table_tokenizer,
+                          prepare=True)
+        QueryTableDataset(data_dir=self.data_dir, data_type='test',
+                          query_tokenizer=self.query_tokenizer,
+                          table_tokenizer=self.table_tokenizer,
+                          prepare=True)
 
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
-            table_full = QueryTableDataset(query_file=self.query_file,
-                                           table_file=self.table_file,
-                                           query_tokenizer=self.query_tokenizer,
-                                           table_tokenizer=self.table_tokenizer,
-                                           )
-            self.train, self.valid = random_split(table_full, [2700, 420])
+            table_full = QueryTableDataset(data_dir=self.data_dir, data_type='train')
+            self.train, self.valid = random_split(table_full, [55, 5])
 
         if stage == 'test' or stage is None:
-            self.test = QueryTableDataset(query_file=self.query_file, table_file=self.table_file)
+            self.test = QueryTableDataset(data_dir=self.data_dir, data_type='test')
 
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.train_batch_size,
-                          shuffle=True, collate_fn=query_table_collate_fn,
-                          num_workers=4)
+                          shuffle=True, collate_fn=query_table_collate_fn)
 
     def val_dataloader(self):
         return DataLoader(self.valid, batch_size=self.valid_batch_size,
-                          collate_fn=query_table_collate_fn,
-                          num_workers=4)
+                          collate_fn=query_table_collate_fn)
 
     def test_dataloader(self):
-        return DataLoader(self.test, batch_size=self.test_batch_size, num_workers=4)
+        return DataLoader(self.test, batch_size=self.test_batch_size)
 
 
 if __name__ == "__main__":
-    data_module = QueryTableDataModule()
+    args = argparse.Namespace()
+    args.data_dir = 'data'
+    args.bert_path = 'bert-base-uncased'
+    args.tabert_path = 'model/tabert_base_k3/model.bin'
+    args.train_batch_size = 2
+    args.valid_batch_size = 2
+
+    data_module = QueryTableDataModule(args)
+    data_module.prepare_data()
     data_module.setup('fit')
     for batch in data_module.train_dataloader():
         print(batch[0])
--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
 import json
-import pickle
+import os
 import random
 from collections import defaultdict
+from pathlib import Path
 
 import torch
 from torch.utils.data import DataLoader, Dataset
@@ -11,62 +12,19 @@ from table_bert import Table, Column, TableBertModel
 
 
 class QueryTableDataset(Dataset):
-    def __init__(self, query_file, table_file, query_tokenizer, table_tokenizer,
-                 max_query_length=15, prepare=True
-                 ):
+    def __init__(self, data_dir: str = '.data', data_type: str = 'train',
+                 query_tokenizer=None, table_tokenizer=None, max_query_length=15,
+                 prepare=False):
         super().__init__()
+        self.data_dir = data_dir
+        self.query_file = data_type + '.query'
+        self.table_file = data_type + '.table'
 
         if prepare:
-            self.query = []
-            with open(query_file, 'r') as f:
-                for line in f.readlines():
-                    id, query = line.strip().split("\t")
-                    # TODO: 수정해야할 부분, 불필요하게 같은 질의에 대해 encode_plus를 수행함.
-                    query_tokenized = query_tokenizer.encode_plus(query,
-                                                                  max_length=max_query_length,
-                                                                  padding='max_length',
-                                                                  truncation=True,
-                                                                  return_tensors="pt"
-                                                                  )
-                    self.query.append((id, query_tokenized))
+            self.prepare(data_dir, data_type, query_tokenizer, table_tokenizer, max_query_length)
 
-            self.pos_tables, self.neg_tables = defaultdict(list), defaultdict(list)
-            with open(table_file, 'r') as f:
-                for line in f.readlines():
-                    if not line.strip():
-                        break
-
-                    # 테이블 기본 Meta data 파싱
-                    jsonStr = json.loads(line)
-                    tableId = jsonStr['docid']
-                    qid = jsonStr['qid']
-                    rel = jsonStr['rel']
-
-                    # Raw Json 파싱
-                    raw_json = json.loads(jsonStr['table']['raw_json'])
-                    title = raw_json['pgTitle']
-                    secTitle = raw_json['secondTitle']
-                    hRow = raw_json['numHeaderRows']
-                    row = raw_json['numDataRows']
-                    col = raw_json['numCols']
-                    caption = raw_json['caption']
-                    heading = raw_json['title']
-                    body = raw_json['data']
-
-                    if col == 0 or row == 0:
-                        continue
-
-                    column_rep = Table(id=title,
-                                       header=[Column(h.strip(), 'text') for h in heading],
-                                       data=body
-                                       ).tokenize(table_tokenizer)
-                    caption_rep = table_tokenizer.tokenize(caption)
-                    if rel == '0':
-                        self.neg_tables[qid].append((tableId, column_rep, caption_rep))
-                    else:
-                        self.pos_tables[qid].append((tableId, column_rep, caption_rep))
-
-            # pickle.dump([query, pos_tables, neg_tables], open("data/data.pkl", "wb"))
+        self.query = torch.load(os.path.join(self.processed_folder, self.query_file))
+        self.pos_tables, self.neg_tables = torch.load(os.path.join(self.processed_folder, self.table_file))
 
     def __len__(self):
         return len(self.query)
@@ -78,6 +36,84 @@ class QueryTableDataset(Dataset):
         tid1, pos_column_rep, pos_caption_rep = random.choice(pos_tables)
         tid2, neg_column_rep, neg_caption_rep = random.choice(self.neg_tables[qid])
         return self.query[index][1], pos_column_rep, pos_caption_rep, neg_column_rep, neg_caption_rep
+
+    def prepare(self, data_dir, data_type, query_tokenizer, table_tokenizer, max_query_length):
+        if self._check_exists():
+            return
+
+        processed_dir = Path(self.processed_folder)
+        processed_dir.mkdir(exist_ok=True)
+
+        if not (query_tokenizer and table_tokenizer):
+            raise RuntimeError('Tokenizers are not found.' +
+                               ' You must set query_tokenizer and table_tokenizer')
+
+        print('Processing...')
+        query_dict = defaultdict()
+        pos_tables, neg_tables = defaultdict(list), defaultdict(list)
+
+        path = Path(data_dir + '/' + data_type + '.jsonl')
+        with open(path) as f:
+            for line in f.readlines():
+                if not line.strip():
+                    break
+
+                # 테이블 기본 Meta data 파싱
+                jsonStr = json.loads(line)
+                tableId = jsonStr['docid']
+                query = jsonStr['query']
+                qid = jsonStr['qid']
+                rel = jsonStr['rel']
+
+                if qid not in query_dict:
+                    query_tokenized = query_tokenizer.encode_plus(query,
+                                                                  max_length=max_query_length,
+                                                                  padding='max_length',
+                                                                  truncation=True,
+                                                                  return_tensors="pt"
+                                                                  )
+                    query_dict[qid] = query_tokenized
+
+                # Raw Json 파싱
+                raw_json = json.loads(jsonStr['table']['raw_json'])
+                title = raw_json['pgTitle']
+                secTitle = raw_json['secondTitle']
+                hRow = raw_json['numHeaderRows']
+                row = raw_json['numDataRows']
+                col = raw_json['numCols']
+                caption = raw_json['caption']
+                heading = raw_json['title']
+                body = raw_json['data']
+
+                if col == 0 or row == 0:
+                    continue
+
+                column_rep = Table(id=title,
+                                   header=[Column(h.strip(), 'text') for h in heading],
+                                   data=body
+                                   ).tokenize(table_tokenizer)
+                caption_rep = table_tokenizer.tokenize(caption)
+                if rel == '0':
+                    neg_tables[qid].append((tableId, column_rep, caption_rep))
+                else:
+                    pos_tables[qid].append((tableId, column_rep, caption_rep))
+
+        queries = [(k, v) for k, v in query_dict.items()]
+        tables = (pos_tables, neg_tables)
+        with open(os.path.join(processed_dir, self.query_file), 'wb') as f:
+            torch.save(queries, f)
+        with open(os.path.join(processed_dir, self.table_file), 'wb') as f:
+            torch.save(tables, f)
+
+        print('Done!')
+
+    @property
+    def processed_folder(self):
+        return os.path.join(self.data_dir, 'processed')
+
+    def _check_exists(self):
+        return (os.path.exists(os.path.join(self.processed_folder, self.query_file)) and
+                os.path.exists(os.path.join(self.processed_folder, self.table_file)))
 
 
 def query_table_collate_fn(batch):
@@ -95,15 +131,26 @@ def query_table_collate_fn(batch):
 
 
 if __name__ == "__main__":
-    query_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    bert_model = BertModel.from_pretrained('bert-base-uncased')
-    table_model = TableBertModel.from_pretrained('model/tabert_base_k3/model.bin')
-    table_tokenizer = table_model.tokenizer
+    # query_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # bert_model = BertModel.from_pretrained('bert-base-uncased')
+    # table_model = TableBertModel.from_pretrained('model/tabert_base_k3/model.bin')
+    # table_tokenizer = table_model.tokenizer
+    #
+    # dataset = QueryTableDataset(data_dir='data',
+    #                             data_type='train',
+    #                             query_tokenizer=query_tokenizer,
+    #                             table_tokenizer=table_tokenizer,
+    #                             prepare=True
+    #                             )
+    # dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=query_table_collate_fn)
+    # for d in dataloader:
+    #     print(d)
+    #     break
 
-    dataset = QueryTableDataset(query_file='data/sample_queries.txt',
-                                table_file='data/sample.json',
-                                query_tokenizer=query_tokenizer,
-                                table_tokenizer=table_tokenizer
+    # Prepare=False
+    dataset = QueryTableDataset(data_dir='data',
+                                data_type='train',
+                                prepare=False
                                 )
     dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=query_table_collate_fn)
     for d in dataloader:
