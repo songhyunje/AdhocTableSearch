7e000a09897f055580f873c5b73554b31b2de853
Devhyung <khuphj@gmail.com>
2020-12-07 02:46:02 +0000

    Add train.sh, Concatmodel

---
+++ train.sh
@@ -0,0 +1,16 @@
+EPOCH=7
+SEED=20200401
+python trainer.py --data_dir ./data \
+  --bert_path bert-base-uncased \
+  --tabert_path ./model/tabert_base_k3/model.bin \
+  --gpus 1 \
+  --precision 32 \
+  --max_epochs ${EPOCH} \
+  --do_train \
+  --do_predict \
+  --train_batch_size 4 \
+  --valid_batch_size 4 \
+  --test_batch_size 1 \
+  --seed ${SEED} \
+  --output_dir ./1201_5_E${EPOCH}_${SEED} \
+  --output_file ./data/result1201_5_E${EPOCH}_${SEED}.csv \
